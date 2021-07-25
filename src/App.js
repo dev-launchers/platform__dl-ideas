0f942787b47a65e6cf69c38f5d4bbe0f181cd40f
han1ya <emailhaniya@gmailcom>
2021-07-24 21:28:45 -0500

    make tabletop call and display sheet data without any styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
-import "./styles.css";
+import React, { useEffect, useState } from "react";
+import Tabletop from "tabletop";
 
 export default function App() {
+  const [data, setData] = useState([]);
+
+  let getSheetsData = () => {
+    Tabletop.init({
+      key: "https://docs.google.com/spreadsheets/d/16RglFZM0lMvsGUmSV3QKti_cMsXSIFp6j7zMvTVxDlA/edit?usp=sharing",
+      callback: showInfo,
+      simpleSheet: true,
+    })
+      .then((data) => {
+        console.log(data);
+      })
+
+      .catch((err) => {
+        console.warn(err);
+      });
+  };
+  React.useEffect(getSheetsData, []);
+
+  function showInfo(data, tabletop) {
+    let tempData = [];
+    data.forEach(function (entry) {
+      tempData.push({
+        projectIdea: entry.projectIdea,
+        projectAudience: entry.targetAudience,
+      });
+    });
+    console.log(tempData);
+    setData(tempData);
+  }
   return (
-    <div className="App">
-      <h1>DL-edu</h1>
-      <h2>Welcome to the project</h2>
-      <h2>GG WP</h2>
-    </div>
+    <>
+      <h1>data from google sheets</h1>
+      <div>
+        {data.map(({ projectIdea, projectAudience }) => {
+          return (
+            <div key={`${projectIdea}_${projectAudience}`}>
+              {projectIdea}
+              <br />
+              <br />
+              {projectAudience}
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
