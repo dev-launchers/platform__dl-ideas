feea76ed164c95f4954f6434cd6ca7f58b747eae
cpark307 <cpark307@gmail.com>
2021-08-15 22:50:44 -0400

    Connected Form to GoogleSheets

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import { StrictMode } from "react";
-import ReactDOM from "react-dom";
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css';
 
-import App from "./App";
+import App from './App';
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
