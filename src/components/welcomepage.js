1ca36d87c268544add7e65d27e25e9c9d43922ea
ViniciusBonani <viniciusbonani@hotmail.com>
2022-06-01 19:41:27 -0300

    welcome page text adjust

--- src/components/welcomepage.js
+++ src/components/welcomepage.js
@@ -5,9 +5,8 @@ import SubmitIdea from '../components/SubmitIdea/SubmitIdea';
 export default function WelcomePage() {
   return (
     <div id="welcomeWrapper">
-      <h1>DL-edu</h1>
+      <h1>DL-Ideas</h1>
       <h2>Welcome to the project</h2>
-      <h2>GG WP</h2>
       <SubmitIdea />
     </div>
   )
