eb71cb36c9a15e7d8a4d9cc94f42d29a7c3d084a
Kris Gano <kris@pyxld.com>
2022-06-28 19:57:43 -0500

    Implement some basic styling to cards page

--- src/components/Card/HeaderCard/StyledHeaderCard.js
+++ src/components/Card/HeaderCard/StyledHeaderCard.js
@@ -1,9 +1,11 @@
 import styled from "styled-components";
 
 const HeaderCard = styled.div`
-  background-color: #59687B;
+  background-color: ${({ theme }) => theme.colors.NEUTRAL_1};
   padding: 19px 21.46px 8px 29.85px;
   height: 160px;
+  border-bottom-left-radius: 2rem;
+  border-bottom-right-radius: 2rem;
 `;
 
 export default HeaderCard;
